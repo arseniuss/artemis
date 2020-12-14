b1f8ce47215708a39671575e0cedf72b302840c8
Armands Arseniuss Skolmeisters <arseniuss@arseniuss.id.lv>
2020-12-14 04:08:40 +0200

    Graphics: Added display lookup

--- game/source/Graphics/Context.cpp
+++ game/source/Graphics/Context.cpp
@@ -18,6 +18,8 @@
 
 #include <dlfcn.h>
 
+#include <iostream>
+
 #include <SDL2/SDL_image.h>
 
 #include <Graphics/Context.hpp>
@@ -52,14 +54,36 @@ shared_ptr<Context> Context::Create(const string& name, const string& title) {
 }
 
 Context::Context(const std::string& title, SDL_WindowFlags flags) {
+    SDL_DisplayMode current;
+    int use_window = -1;
+
     if (SDL_InitSubSystem(SDL_INIT_VIDEO) < 0) {
         throw runtime_error(string("Cannot init SDL video: ") + SDL_GetError());
+    }
+
+    for (int i = 0; i < SDL_GetNumVideoDisplays(); ++i) {
+        int should_be_zero = SDL_GetCurrentDisplayMode(i, &current);
+
+        if (should_be_zero != 0) {
+            std::cout << "Couldn't get display mode for video display #" << i
+                    << ": " << SDL_GetError() << std::endl;
+        } else {
+            std::cout << "Display #" << i << ": current display mode is " <<
+                    current.w << "x" << current.h << "@" <<
+                    current.refresh_rate << std::endl;
+            if (use_window < 0) use_window = i;
+        }
+    }
+
+    if (use_window < 0) {
+        throw runtime_error(string("Cannot find display!"));
     }
 
     IMG_Init(IMG_INIT_PNG);
 
     _window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED,
-            SDL_WINDOWPOS_CENTERED, 800, 600, flags | SDL_WINDOW_SHOWN);
+            SDL_WINDOWPOS_CENTERED, current.w, current.h,
+            flags | SDL_WINDOW_SHOWN | SDL_WINDOW_MAXIMIZED);
     if (_window == nullptr) {
         throw runtime_error("Cannot create window!");
     }
